cbb0fb2c1222e88427588fe6be2bf4901d488071
chengshuyi <1541189572@qq.com>
2018-09-11 11:31:03 +0800

    FPS FILE_VIDEO

--- include/config.h
+++ include/config.h
@@ -4,7 +4,7 @@
 #define FRAME_PIX_WIDTH     640
 #define FRAME_PIX_HEIGHT    480
 #define PIXELFORMAT         V4L2_PIX_FMT_YUYV
-#define FILE_VIDEO          "/dev/video0"
+#define FILE_VIDEO          "/dev/video1"
 /**
  * @brief 
  * 
@@ -18,9 +18,9 @@
  */
 
 //#define DEST_IP             {127,0,0,1}
-#define DEST_IP             {192,168,137,3}
+#define DEST_IP             {192,168,137,2}
 #define LOCAL_PORT          3000
 #define REMOTE_PORT         4000
-#define FPS                 100
+#define FPS                 30
 
 #endif
